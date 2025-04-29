5ffe625b451168306244687a183cbb78a73917c6
Vrindaverse <nikhilmahalik78@gmail.com>
2025-04-30 02:47:24 +0530

    solve karo

---
+++ bhawna.py
@@ -0,0 +1,3 @@
+##ram is playing cricket and he will be considered raja if he scores 100 or more and chutiya if he scores less thann 40 and "good" player if he scores more than or equal to 40 and less than 100
+##input : 5,150,20,40,50,69
+#output:raja,cutiya,good,good,good
