cc78ed0a28ed74e8d5ae3db8afddaac07bc869c5
Vrindaverse <nikhilmahalik78@gmail.com>
2025-04-30 02:53:54 +0530

    solve karna moti

--- bhawna.py
+++ bhawna.py
@@ -1,3 +1,4 @@
 ##ram is playing cricket and he will be considered raja if he scores 100 or more and chutiya if he scores less thann 40 and "good" player if he scores more than or equal to 40 and less than 100
 ##input : 5,150,20,40,50,69
 #output:raja,cutiya,good,good,good
+# first input will be test cases 
